663a66b20092ba569b4a5dfa8180f914d287e516
amrithadevadiga <44697236+amrithadevadiga@users.noreply.github.com>
2019-08-12 10:49:15 +0530

    Update 1_configure.sh

--- HDP_upgrade_scenario/environments/bluedata-workbench/build/1_configure.sh
+++ HDP_upgrade_scenario/environments/bluedata-workbench/build/1_configure.sh
@@ -10,7 +10,5 @@ yum install -y epel-release
 yum install -y python-requests python-setuptools python-argparse python-pip
 
 
-wget https://www.dropbox.com/s/2mh0pzsq1092xr8/bdcatalog-centos7-bluedata-hdp25-ambari-3.3.bin.tar.gz
-tar xvf bdcatalog-centos7-bluedata-hdp25-ambari-3.3.bin.tar.gz
-chmod +x bdcatalog-centos7-bluedata-hdp25-ambari-3.3.bin
+
 
